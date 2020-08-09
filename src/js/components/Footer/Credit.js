b17f8aaebed73be8e0117c0bb414ca170e781167
sawayakayaka <lavi_heart@icloud.com>
2020-08-09 22:38:19 +0900

    study-1

---
+++ src/js/components/Footer/Credit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Credit extends React.Component {
+  render() {
+    return (
+      <footer>
+        study react!
+      </footer>
+    );
+  }
+}
