4b0542b1ac4cbfac17eca9cab9477c72ee1aea1d
sawayakayaka <lavi_heart@icloud.com>
2020-08-09 23:27:16 +0900

    study-2

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,11 +3,19 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = { title: "welcome" };
+  }
+
+  changeTitle(title) {
+    this.setState({ title: title });
+  }
 
   render() {
     return (
       <div>
-        <Header />
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
         <Footer />
       </div>
     );
